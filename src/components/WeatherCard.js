03b4c4d9720f9585f61b7fb035dd33ea77ab2c4b
Aaron Schnall <aschnall@Aarons-Air.fios-router.home>
2020-11-04 10:23:49 -0500

    uploading project

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,34 +1,5 @@
 import React from 'react';
 
-// const test = (response) => {  
-//   const today = new Date();
-//   const day = 60 * 60 * 24 * 1000;
-
-//   const dateBins = {};
-//   const nBins = 6; // there can be reports for up to 6 distinct dates
-
-//   for (let i = 0; i < nBins; i++) {
-//       // set up a bin (empty array) for each date
-//       const date = new Date(today.getTime() + i * day);
-//       dateBins[date.getDate()] = [];
-//   }
-
-//   const reports = response;
-//   for (const report of reports) {
-//       const reportDate = new Date(report.dt * 1000).getDate();
-//       dateBins[reportDate].push(report);
-//   }
-//   return dateBins;
-// }
-
-// const test1 = (day, response) => {
-//     const test3 = test(response);
-//     let max = test3[day].reduce((max, current) => (current.main.temp_max > max ? current.main.temp_max : max),0)
-//     return max;
-// }
-
-
-
 const WeatherCard = ({ date, type, icon, temp }) => {
 	
 	return(
@@ -45,33 +16,5 @@ const WeatherCard = ({ date, type, icon, temp }) => {
 };
 
 
-export default WeatherCard;
-
-// // test() {    
-//     const today = new Date();
-//     const day = 60 * 60 * 24 * 1000;
-
-//     const dateBins = {};
-//     const nBins = 6; // there can be reports for up to 6 distinct dates
-
-//     for (let i = 0; i < nBins; i++) {
-//         // set up a bin (empty array) for each date
-//         const date = new Date(today.getTime() + i * day);
-//         dateBins[date.getDate()] = [];
-//     }
-
-//     const reports = weatherData.list;
-//     for (const report of reports) {
-//         const reportDate = new Date(report.dt * 1000).getDate();
-//         dateBins[reportDate].push(report);
-//     }
-//     return dateBins;
-// // }
-
-// // function test1(day) {
-// //     const test3 = test();
-// //     let max = test3[day].reduce((max, current) => (current.main.temp_max > max ? current.main.temp_max : max),0)
-// //     return max;
-// // }
 
 
--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -27,36 +27,3 @@ export default WeatherList;
 
 // class WeatherList extends React.Component {
 	
-// 	state = {list: []};
-
-// 	componentDidMount() {
-// 		this.setState({list:this.props.temp})
-// 	}
-	
-// 	weather = () => {
-// 		this.props.weather.map(day => {
-// 			return <WeatherCard 
-// 						date={moment(day.dt*1000).format('ddd, MMM Do')}
-// 						type={day.weather[0].description}
-// 						icon={day.weather[0].icon}
-// 						temp={Math.round((day.main.temp)*9/5 - 459.67)}
-// 						key={day.dt}
-// 						list={this.state.list}
-// 					/>
-// 		});
-// 	}
-
-
-// 	render() {
-// 		return (
-// 			<div className='forecast-box'>
-// 				{this.weather()}
-// 			</div>
-// 		)
-// 	}
-// }
-
-	
-
-// export default WeatherList;
-	
